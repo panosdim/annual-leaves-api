ef7b2ac54403415f0d4abba8dd5f5b9809249168
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2019-01-30 10:11:45 +0200

    Enable CORS

---
+++ config/cors.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['http://localhost:3000', 'leaves.cc.nf'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+];
